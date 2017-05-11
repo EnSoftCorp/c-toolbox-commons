ccbef1947cf62090a248cfecf343decff9e25968
Ben Holland <benjholla@gmail.com>
2017-05-11 12:41:38 -0500

    improving set definitions

---
+++ com.ensoftcorp.open.c.commons/src/com/ensoftcorp/open/c/commons/analysis/SetDefinitions.java
@@ -0,0 +1,53 @@
+package com.ensoftcorp.open.c.commons.analysis;
+
+import com.ensoftcorp.atlas.core.query.Q;
+import com.ensoftcorp.atlas.core.script.Common;
+import com.ensoftcorp.atlas.core.xcsg.XCSG;
+
+/**
+ * Common set definitions which are useful for program analysis
+ * 
+ * @author Ben Holland
+ */
+public final class SetDefinitions {
+
+	// hide constructor
+	private SetDefinitions() {}
+
+	/**
+	 * Types which represent arrays of other types
+	 * 
+	 * NOTE: These nodes are NOT declared by anything. They are outside of any
+	 * project.
+	 */
+	public static Q arrayTypes() {
+		return com.ensoftcorp.open.commons.analysis.SetDefinitions.arrayTypes();
+	}
+
+	/**
+	 * Types which represent language primitive types
+	 * 
+	 * NOTE: These nodes are NOT declared by anything. They are outside of any
+	 * project.
+	 */
+	public static Q primitiveTypes() {
+		return com.ensoftcorp.open.commons.analysis.SetDefinitions.primitiveTypes();
+	}
+
+	/**
+	 * Everything declared under any of the known API projects, if they are in
+	 * the index.
+	 */
+	public static Q libraries() {
+		return com.ensoftcorp.open.commons.analysis.SetDefinitions.libraries();
+	}
+	
+	/**
+	 * Everything in the universe which is part of the app (not part of the
+	 * libraries, or any "floating" nodes).
+	 */
+	public static Q app() {
+		return com.ensoftcorp.open.commons.analysis.SetDefinitions.app();
+	}
+
+}
